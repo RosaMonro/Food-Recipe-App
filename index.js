acc75deef724b3886491ebafe606a6467047b6c8
RosaMonro <rosamaria.monro@gmail.com>
2023-07-09 17:53:26 +0200

    recipeDiv

--- index.js
+++ index.js
@@ -8,7 +8,23 @@ let searchQuery = ''; //Para almacenar el valor del campo de búsqueda. Al inici
 const fetchAPI = async (query) => {
     const data = await fetch (`http://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
     const response = await data.json();
-    console.log(response);
+
+    response.meals.forEach (meal => { //iteramos cada objeto (meal) del array de comidas (response.meals) y ejecutamos la función para cada elemento
+        const recipeDiv = document.createElement ('div');
+        recipeDiv.classList.add('recipe');
+        recipeDiv.innerHTML = `
+            <div class="card">
+
+                <img src="${meal.strMealThumb}" alt="">
+                <div class="card__info">
+                    <a class="h4  card__tittle" href="">Vista de la receta</a>
+                    <p class="card-data  font-size-16-xxs">Calorias: 100</p>
+                </div>
+
+            </div>
+        `;
+        searchResultDiv.appendChild(recipeDiv);
+    });
 }
 
 searchForm.addEventListener('submit', (e) => { //queremos que "escuche" el evento en 'submit' cuando se haga click en Buscar. (e) = (event)
@@ -19,12 +35,13 @@ searchForm.addEventListener('submit', (e) => { //queremos que "escuche" el event
     //querySelector('input') busca el campo de entrada dentro del formulario.
     //.value obtiene el texto que el usuario ha ingresado en ese campo y lo guarda en la variable searchQuery.
     //A medida que el usuario interactúa con el formulario y envía una consulta de búsqueda, el código JavaScript actualiza el valor de searchQuery con el contenido del campo de entrada del formulario
-    fetchAPI()
+    fetchAPI(searchQuery); //se pasa searchQuery como argumento al llamar a fetchAPI(searchQuery).Así, el valor del campo de búsqueda se pasa a la función fetchAPI() y se utilizará en la URL de la API.
 })
 
 // async function fetchAPI (){
 //     const baseURL = `https://api.edamam.com/api/recipes/v2?q=pizza&app_is=${APP_ID}&app_key=${APP_key}`; //usamos `` para poder manipular la cadena. Al utilizar los backticks, se puede incluir una expresión ${...} dentro de la cadena de texto para interpolación de variables.
 //     const response = await fetch(baseURL); //await para pausar la ejecución del código hasta que se complete una operación asincrónica y se resuelva una promesa.
-//     console.log(response);
+//     const data = await response.json();
+//     console.log(data);
 // }
 
